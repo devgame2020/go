0f746107faa164bc1937b2be93932222122bf333
devgame2020 <scym12@gmail.com>
2025-06-10 21:40:00 +0900

    4장 source code

--- basicfeatures/main.go
+++ basicfeatures/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"       // 기본 입출력 패키지
 	"math/rand" // 난수생성 패키지
+	"sort"
 )
 
 func main() {
@@ -12,7 +13,10 @@ func main() {
 	// f4_102()
 	// f4_105()
 	// f4_106()
-	f4_107()
+	// f4_107()
+	// f4_112()
+	// f4_114()
+	f4_115()
 }
 
 func f4_01() {
@@ -115,13 +119,59 @@ func f4_107() {
 	fmt.Println("First:", first)
 	fmt.Println("Second:", second)
 
+	// 포인터 역참조
 	first2 := 100
-	var second2 *int = &first
-	second3 := &first
+	var second2 *int = &first // 포인터변수 second2에 first의 주소값을 저장한다.
+	second3 := &first         //
 
 	first2++
 	fmt.Println("First2:", first2)
 	fmt.Println("Second2:", second2)
 	fmt.Println("Second2:", *second2)
 	fmt.Println("Second3:", *second3)
+
+	var myPointer *int
+	myPointer = second3
+	*myPointer++
+
+	fmt.Println("First:", first)
+	fmt.Println("myPointer:", *myPointer)
+}
+
+func f4_112() {
+	first := 100
+	var second *int
+
+	fmt.Println(second)
+	second = &first
+	// fmt.Println(second)
+	fmt.Println(second == nil)
+}
+
+func f4_114() {
+	first := 100
+	second := &first
+	third := &second
+
+	fmt.Println(first)
+	fmt.Println(*second)
+	fmt.Println(**third)
+}
+
+func f4_115() {
+	// secondName 은 names[1]의 value를 저장하고 있기때문에 sort를 하더라도 값이 변하지 않는다.
+	names := [3]string{"Alice", "Charlie", "Bob"}
+	secondName := names[1]
+	fmt.Println(secondName)
+	sort.Strings(names[:])
+	fmt.Println(secondName)
+	fmt.Println(names)
+
+	// secondPosition은 names[1]의 주소값을 저장하고 있기때문에 정렬후 names[1]의 주소에 해당하는 값이 변경될수있다.
+	names2 := [3]string{"Alice", "Charlie", "Bob"}
+	secondPosition := &names2[1]
+	fmt.Println(*secondPosition)
+	sort.Strings(names2[:])
+	fmt.Println(*secondPosition)
+
 }
