c684ba2f4345fb1d23f97f23a571258815452b28
devgame2020 <scym12@gmail.com>
2025-07-03 07:08:15 +0000

    code

--- gameserver/middleware/auto.go
+++ gameserver/middleware/auto.go
@@ -52,61 +52,8 @@ func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 		fmt.Println("AuthMiddleware:userId:", claims["user_id"])
 		c.Set("username", claims["username"])
 
-		// 예: userID를 claim에서 추출하고 context에 저장
-		// userIDFloat, ok := claims["user_id"].(float64)
-
-		/*
-			token := strings.TrimPrefix(auth, "Bearer ")
-			fmt.Println(auth)
-			fmt.Println(token)
-			claims, err := utils.ParseToken(token)
-			userID := claims.UserID
-			if err != nil {
-				fmt.Println("error", "Invalid token")
-				return c.JSON(http.StatusUnauthorized, echo.Map{"error": "Invalid token"})
-			}
-
-			// 컨텍스트에 userID 저장
-			fmt.Println("AuthMiddleware:userId:", userID)
-			c.Set("userID", userID)
-		*/
 
 		return next(c)
 	}
 }
 
-/*
-func JWTAuthMiddleware() echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			auth := c.Request().Header.Get("Authorization")
-			if auth == "" || !strings.HasPrefix(auth, "Bearer ") {
-				return c.JSON(http.StatusUnauthorized, echo.Map{"error": "Missing or invalid token"})
-			}
-
-			tokenString := strings.TrimPrefix(auth, "Bearer ")
-			token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-				return []byte(os.Getenv("JWT_SECRET")), nil
-			})
-
-			if err != nil || !token.Valid {
-				return c.JSON(http.StatusUnauthorized, echo.Map{"error": "Invalid token"})
-			}
-
-			claims, ok := token.Claims.(jwt.MapClaims)
-			if !ok {
-				return c.JSON(http.StatusUnauthorized, echo.Map{"error": "Invalid token claims"})
-			}
-
-			// 예: userID를 claim에서 추출하고 context에 저장
-			userIDFloat, ok := claims["user_id"].(float64)
-			if !ok {
-				return c.JSON(http.StatusUnauthorized, echo.Map{"error": "Invalid user_id in token"})
-			}
-			c.Set("userID", int(userIDFloat))
-
-			return next(c)
-		}
-	}
-}
-*/
