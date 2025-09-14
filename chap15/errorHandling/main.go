50b02c1a8e989ee6e176a36b39441b1b0404b479
devgame2020 <scym12@gmail.com>
2025-09-14 17:39:37 +0900

    chap15 , 고루틴 패닉 복구  (15-19)

--- chap15/errorHandling/main.go
+++ chap15/errorHandling/main.go
@@ -2,29 +2,51 @@ package main
 
 import "fmt"
 
-func main() {
+type CategoryCountMessage struct {
+	Category      string
+	Count         int
+	TerminalError interface{}
+}
+
+func processCategories(categories []string, outChan chan<- CategoryCountMessage) {
 	defer func() {
+		fmt.Println("defer call")
 		if arg := recover(); arg != nil {
-			if err, ok := arg.(error); ok {
-				fmt.Println("Error:", err.Error())
-				panic(err)
-			} else if str, ok := arg.(string); ok {
-				fmt.Println("Message:", str)
-			} else {
-				fmt.Println("Panic recovered")
+			fmt.Println(arg)
+			fmt.Println("send channel")
+			outChan <- CategoryCountMessage{
+				TerminalError: arg,
 			}
+			close(outChan)
 		}
 	}()
-
-	categories := []string{"Watersports", "Chess", "Running"}
 	channel := make(chan ChannelMessage, 10)
 	go Products.TotalPriceAsync(categories, channel)
 	for message := range channel {
 		if message.CategoryError == nil {
-			fmt.Println(message.Category, "Total:", ToCurrency(message.Total))
+			outChan <- CategoryCountMessage{
+				Category: message.Category,
+				Count:    int(message.Total),
+			}
 		} else {
 			panic(message.CategoryError)
-			//fmt.Println(message.Category, "(no such category)")
+		}
+	}
+	close(outChan)
+}
+
+func main() {
+
+	categories := []string{"Watersports", "Chess", "Running"}
+
+	channel := make(chan CategoryCountMessage)
+	go processCategories(categories, channel)
+
+	for message := range channel {
+		if message.TerminalError == nil {
+			fmt.Println(message.Category, "Total:", message.Count)
+		} else {
+			fmt.Println("A terminal error occured")
 		}
 	}
 }
