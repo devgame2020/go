83253c9dbc3513740b2b35c67e4ccba068eecb02
devgame2020 <scym12@gmail.com>
2025-09-17 03:18:30 +0900

    chap14 : 채널을 사용하여 결과를 수집 (14-13)

--- chap14/concurrency/main.go
+++ chap14/concurrency/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	fmt.Println("main function started")
 	CalcStoreTotal(Products)
-	time.Sleep(time.Second * 5)
 	fmt.Println("main function complete")
 }
