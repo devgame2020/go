de62f850ee02e8f7c299cb03503b38853811a6b7
devgame2020 <scym12@gmail.com>
2025-09-17 03:35:21 +0900

    chap14 : 채널사용시 채널수신될때가지, Block되는것을 확인 (14-14)

--- chap14/concurrency/operations.go
+++ chap14/concurrency/operations.go
@@ -11,18 +11,25 @@ func CalcStoreTotal(data ProductData) {
 	for category, group := range data {
 		go group.TotalPrice(category, channel)
 	}
+	time.Sleep(time.Second * 5)
+	fmt.Println("-- Starting to receive from channel")
 	for i := 0; i < len(data); i++ {
-		storeTotal += <-channel
+		fmt.Println("-- channel read pending")
+		value := <-channel
+		fmt.Println("-- channel read complete", value)
+		storeTotal += value
+		time.Sleep(time.Second)
 	}
 	fmt.Println("Total:", ToCurrency(storeTotal))
 }
 func (group ProductGroup) TotalPrice(category string, resultChannel chan float64) {
 	var total float64
 	for _, p := range group {
-		fmt.Println(category, "product:", p.Name)
+		//fmt.Println(category, "product:", p.Name)
 		total += p.Price
 		time.Sleep(time.Millisecond * 100)
 	}
-	fmt.Println(category, "subtotal:", ToCurrency(total))
+	fmt.Println(category, "channel sending", ToCurrency(total))
 	resultChannel <- total
+	fmt.Println(category, "channel send complete")
 }
