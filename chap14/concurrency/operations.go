d2587918cef8e153f025dc140d8a60b4659a340e
devgame2020 <scym12@gmail.com>
2025-09-17 03:02:49 +0900

    chap14 : 고루틴 추가

--- chap14/concurrency/operations.go
+++ chap14/concurrency/operations.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func CalcStoreTotal(data ProductData) {
 	var storeTotal float64
 	for category, group := range data {
-		storeTotal += group.TotalPrice(category)
+		// storeTotal += group.TotalPrice(category)
+		go group.TotalPrice(category)
 	}
 	fmt.Println("Total:", ToCurrency(storeTotal))
 }
@@ -13,6 +17,7 @@ func (group ProductGroup) TotalPrice(category string) (total float64) {
 	for _, p := range group {
 		fmt.Println(category, "product:", p.Name)
 		total += p.Price
+		time.Sleep(time.Millisecond * 100)
 	}
 	fmt.Println(category, "subtotal:", ToCurrency(total))
 	return
