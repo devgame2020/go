fe58c3771ab8efdecc379e41b41a257cabe61c36
devgame2020 <scym12@gmail.com>
2025-09-17 20:07:19 +0900

    chap14 : 채널 인수 방향 제한 (14-26)

--- chap14/concurrency/main.go
+++ chap14/concurrency/main.go
@@ -4,18 +4,22 @@ import (
 	"fmt"
 )
 
-func main() {
-	/*
-		fmt.Println("main function started")
-		CalcStoreTotal(Products)
-		fmt.Println("main function complete")
-	*/
-
-	dispatchChannel := make(chan DispatchNotification, 100)
-	go DispatchOrders(dispatchChannel)
-	for details := range dispatchChannel {
+func receiveDispatches(channel <-chan DispatchNotification) {
+	for details := range channel {
 		fmt.Println("Dispatch to", details.Customer, ":", details.Quantity,
 			"x", details.Product.Name)
 	}
 	fmt.Println("Channel has been closed")
+}
+
+func main() {
+	// dispatchChannel := make(chan DispatchNotification, 100)
+	// var sendOnlyChannel chan<- DispatchNotification = dispatchChannel
+	// var receiveOnlyChannel <-chan DispatchNotification = dispatchChannel
+	// go DispatchOrders(sendOnlyChannel)
+	// receiveDispatches(receiveOnlyChannel)
+
+	dispatchChannel := make(chan DispatchNotification, 100)
+	go DispatchOrders(chan<- DispatchNotification(dispatchChannel))
+	receiveDispatches((<-chan DispatchNotification)(dispatchChannel))
 }
