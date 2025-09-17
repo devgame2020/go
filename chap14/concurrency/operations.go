d2bcb511cd83651658cc2fc422be6bfe090ac4d1
devgame2020 <scym12@gmail.com>
2025-09-17 19:14:16 +0900

    chap14 : 채널버퍼 사용 (14-16)

--- chap14/concurrency/operations.go
+++ chap14/concurrency/operations.go
@@ -7,14 +7,15 @@ import (
 
 func CalcStoreTotal(data ProductData) {
 	var storeTotal float64
-	var channel chan float64 = make(chan float64)
+	var channel chan float64 = make(chan float64, 2)
 	for category, group := range data {
 		go group.TotalPrice(category, channel)
 	}
 	time.Sleep(time.Second * 5)
 	fmt.Println("-- Starting to receive from channel")
 	for i := 0; i < len(data); i++ {
-		fmt.Println("-- channel read pending")
+		fmt.Println("-- channel read pending",
+			len(channel), "items in buffer, size", cap(channel))
 		value := <-channel
 		fmt.Println("-- channel read complete", value)
 		storeTotal += value
