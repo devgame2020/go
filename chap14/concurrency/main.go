5e1bc362d8b4b9e22458020ce0a9405adc059c7b
devgame2020 <scym12@gmail.com>
2025-09-17 19:20:37 +0900

    chap14 : 미정 개수 값 전송 및 수신 (14-18)

--- chap14/concurrency/main.go
+++ chap14/concurrency/main.go
@@ -5,7 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println("main function started")
-	CalcStoreTotal(Products)
-	fmt.Println("main function complete")
+	/*
+		fmt.Println("main function started")
+		CalcStoreTotal(Products)
+		fmt.Println("main function complete")
+	*/
+
+	dispatchChannel := make(chan DispatchNotification, 100)
+	go DispatchOrders(dispatchChannel)
+	for {
+		details := <-dispatchChannel
+		fmt.Println("Dispatch to", details.Customer, ":", details.Quantity,
+			"x", details.Product.Name)
+	}
 }
